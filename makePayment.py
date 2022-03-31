7c1c626d7c89c2152b74dbfd51da3b18a117fb5b
NikkiCJQ <nikki.chua.2020@scis.smu.edu.sg>
2022-03-31 14:56:34 +0800

    update

---
+++ makePayment.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import os, sys
+
+import requests
+from invokes import invoke_http
+
+import amqp_setup
+import pika
+import json
+# AMQP service consumer and provider
+
+app = Flask(__name__)
+CORS(app)
+
+getpayment_URL = "http://localhost:5501/getAmount"
+response = requests.get(getpayment_URL)
+json_data = response.json()
+r = json.dumps(json_data)
+
+# def write_json(data, filename="test.json"):
+#     with open (filename, "w") as datafile:
+#         response = requests.get(getpayment_URL)
+#         json_data = response.json()
+#         print(json_data)
+#         data = json.dumps(json_data)
+#         json.dump(data, datafile, indent=4)
+
+
+@app.route("/make_payment")
+def make_payment():
+    # with open ("test.json") as json_file:
+    #     data = json.load(json_file)
+    #     print(data)
+
+        try:
+            order = json_data
+            print(order)
+            amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="123.invoice", 
+            body=r)
+
+            
+            print('\n------------------------')
+            print('\nresult: ', r)
+            return jsonify(json_data)
+
+        except Exception as e:
+            # Unexpected error in code
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+
+            return jsonify({
+                "code": 500,
+                "message": "place_order.py internal error: " + ex_str
+            }), 500
+
+
+
+    
+
+# Execute this program if it is run as a main script (not by 'import')
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) + " for placing an order...")
+    app.run(host="0.0.0.0", port=5500, debug=True)
+    # Notes for the parameters: 
+    # - debug=True will reload the program automatically if a change is detected;
+    #   -- it in fact starts two instances of the same flask program, and uses one of the instances to monitor the program changes;
+    # - host="0.0.0.0" allows the flask program to accept requests sent from any IP/host (in addition to localhost),
+    #   -- i.e., it gives permissions to hosts with any IP to access the flask program,
+    #   -- as long as the hosts can already reach the machine running the flask program along the network;
+    #   -- it doesn't mean to use http://0.0.0.0 to access the flask program.
--- payment.py
+++ payment.py
@@ -1,54 +1,112 @@
-from unittest import result
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, redirect, render_template, request, render_template
 from flask_cors import CORS
 import json
-
+import amqp_setup
+import pika
+from invokes import invoke_http
 import firebase_admin
-
-
 app = Flask(__name__)
-CORS(app)
 
+CORS(app)
 cred_obj = firebase_admin.credentials.Certificate('esdg9t02-insurance-firebase-adminsdk-umgr1-f4dd6e06a6.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
   'databaseURL':'https://esdg9t02-insurance-default-rtdb.asia-southeast1.firebasedatabase.app/'
   })
 
 from firebase_admin import db
+import datetime
+
+dateee = datetime.datetime.now().date()
+dateee = dateee.strftime("%m-%d-%Y")
 
 ref = db.reference("/")
 customer_ref = ref.child('customer')
+policy_ref = ref.child('Policy')
+
+getCustomerRef = db.reference("/customer/customerID")
+data = getCustomerRef.get()
+
+getPolicies = db.reference("/Policy")
+policydata = getPolicies.get()
 
 
+# policy = policydata["123Africa0103-31-2022"]
+policy1 = policydata
+print(policy1)
+
+amt = ''
+outstandingpolicy = ''
+for (x,y) in policy1.items():
+    print(x,y)
+    if y['PaymentStatus'] == "Outstanding":
+        amt = y["Price"]
+        outstandingpolicy = x
+    elif y['PaymentStatus'] == "Paid":
+        amt = y["OutstandingAmt"]
+print(outstandingpolicy)
+    # if policy["PaymentStatus"] == "Outstanding":
+    #     amt = policy["Price"]
+
+    # elif policy["PaymentStatus"] == "Paid":
+    #     amt = policy["OutstandingAmt"]
+    # print(amt)
+@app.route('/display')
+def display():
+    getPoliciesLate = db.reference("/Policy")
+    policy1data = getPoliciesLate.get()
+    
+    return  jsonify(
+            {
+                "code": 200,
+                "data": policy1data
+            }       
+    )
 
 @app.route("/getpayment/<string:amt>", methods=['POST'])
 def payment(amt):
-    amt = request.get_json()
-    result = json.loads(amt)
-    for i in result:
-        finalamt = result[i]
-    print(finalamt)
-    
-    
-    hopper_ref = customer_ref.child('customerID')
+    print(amt)
+    hopper_ref = policy_ref.child(outstandingpolicy)
     hopper_ref.update({
-            "paymentStatus": "paid",
-            "amount": finalamt
+            "PaymentStatus": "Paid",
+            "PaymentDate":dateee,
+            "OutstandingAmt": 0,
+            "Status":"Active"
     })
-    
-    print(type(result))
-
     return jsonify(
         {
             "data": amt
         }
     ), 201
 
+@app.route('/getAmount')
+def getAmt():
+    getPoliciesLate = db.reference("/Policy")
+    policy1data = getPoliciesLate.get()
+    for (x,y) in policy1data.items():
+        print(x,y)
+        if y['PaymentStatus'] == "Outstanding":
+            amt = y["Price"]
+        elif y['PaymentStatus'] == "Paid":
+            amt = y["OutstandingAmt"]
 
+    return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "Amt":amt,
+                    "details":policy1
+                }
+            }
+        )
 
-
-
-
-
+@app.route('/getDetails')
+def getDetails():
+    
+    return  jsonify(
+            {
+                "code": 200,
+                "data": policy1
+            }       
+    )
 if __name__ == "__main__":
-    app.run(port='5500',debug=True)
+    app.run(host='0.0.0.0', port='5501',debug=True)
